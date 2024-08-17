c146603a21309449cc4655f1f95a8639908bc4c8
Lucas Ferreira <luscafreis@gmail.com>
2024-08-17 15:06:49 -0300

    Implementado JWT Token

---
+++ src/main/java/com/financeiro/gestao/security/JwtTokenProvider.java
@@ -0,0 +1,52 @@
+package com.financeiro.gestao.security;
+
+import com.financeiro.gestao.domain.model.Usuario;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${security.jwt.token.secret-key}")
+    private String jwtSecret;
+
+    @Value("${security.jwt.token.expire-length}")
+    private long jwtExpirationMs;
+
+    public String generateToken(Authentication authentication) {
+
+        Usuario usuario = (Usuario) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationMs);
+
+        return Jwts.builder()
+                .setSubject(usuario.getId().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException | MalformedJwtException | ExpiredJwtException | UnsupportedJwtException | IllegalArgumentException ex) {
+            return false;
+        }
+    }
+}
